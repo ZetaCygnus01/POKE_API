d02b8e6ea2e9f37ec79db70342717ca9ffb563d2
ZetaCygnus01 <122745586+ZetaCygnus01@users.noreply.github.com>
2025-05-15 01:48:44 +0000

    Creando Login y ajustando appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppProvider } from './contexto/contexto';
+import './App.css';
 
+import { supabase } from "./supabase";
 import Menu from './Componentes/Menu'
 import Aleatorios from './Componentes/Aleatorios';
 import Lista from './Componentes/Lista';
@@ -9,8 +11,28 @@ import Capturados from './Componentes/Capturados';
 import Favoritos from './Componentes/Favoritos';
 import Usuarios from './Componentes/Usuarios';
 import Detalle from './Componentes/Detalle';
+import Login from './Componentes/Login';
 
 function App() {
+const [usuario, setUsuario] = useState(null);
+const [cargando, setCargando] = useState(true);
+
+useEffect(() => {
+async function verificarSesion() {
+const { data: { session } } = await supabase.auth.getSession();
+setUsuario(session?.user || null);
+setCargando(false);
+}
+verificarSesion();
+
+// Escucha cambios en la sesión
+supabase.auth.onAuthStateChange((_event, session) => {
+
+setUsuario(session?.user || null);
+});
+}, []);
+
+if (cargando) return <p>Cargando...</p>;
 
   return (
     <AppProvider>
@@ -24,6 +46,7 @@ function App() {
         <Route path="/capturados" element={<Capturados />} />
         <Route path="/favoritos" element={<Favoritos />} />
         <Route path="/detalle/:name" element={<Detalle />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
 
     </Router>
